c6770cb10923b9c84cf5c091e47a3d75a1b5ca8c
Emilix22 <torresdragon@hotmail.com>
2023-12-15 15:30:55 -0300

    modifico el router en characters

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,24 +1,18 @@
 import './App.css'
 import { CharactersContextProvider } from '../../context/charactersContext' 
 import Characters from '../characters/Characters'
-import InfoCharacter from '../infoCharacter/InfoCharacter'
-import { Routes, Route } from 'react-router-dom'
 
 function App() {
 
   return (
-    <CharactersContextProvider>
-      <div className='container'>
-        
-        <h1 className='title'>Rick and Morty App</h1>
-        
-        <Routes>
-          <Route path="/" element={<Characters />} />
-          <Route path="/info/:id" element={<InfoCharacter />} />
-        </Routes>
+    <div className='container'>
+      <h1 className='title'>Rick and Morty App</h1>
+      
+      <CharactersContextProvider>
+        <Characters />        
+      </CharactersContextProvider>
 
-      </div>
-    </CharactersContextProvider>
+    </div>
   )
 }
 
--- src/components/characters/Characters.jsx
+++ src/components/characters/Characters.jsx
@@ -2,32 +2,42 @@ import { React, useContext } from 'react'
 import { CharactersContext } from '../../context/charactersContext'
 import Card from '../card/Card'
 import Pagination from '../pagination/Pagination'
-import "./Characters.css" 
+import InfoCharacter from '../infoCharacter/InfoCharacter'
+import { Routes, Route } from 'react-router-dom'
+import "./Characters.css"
+
 
 function Characters() {
 
     const { characters } = useContext(CharactersContext)
 
     return (
-        <>
-            <Pagination />
-            <div className='characters_container'>
-                {
-                    characters.map((character) => {
-                        return (
-                            <Card
-                            key={character.id}
-                            image={character.image}
-                            name={character.name}
-                            status={character.status}
-                            species={character.species}
-                            id={character.id}
-                            />
-                        )
-                    })
-                }
-            </div>
-        </>
+        <Routes>
+            <Route path='/' element={
+                <>
+                    <Pagination />
+                    <div className='characters_container'>
+                        {
+                            characters.map((character) => {
+                                return (
+                                    <Card
+                                    key={character.id}
+                                    image={character.image}
+                                    name={character.name}
+                                    status={character.status}
+                                    species={character.species}
+                                    id={character.id}
+                                    />
+                                )
+                            })
+                        }
+                    </div>
+                </>} 
+            />
+            <Route path='/info/:id' element={<InfoCharacter />} />
+
+        </Routes>
+        
         
     )
 }
