f6041abd09180522371a0daa9d5cdfeb4130a23d
Emilix22 <torresdragon@hotmail.com>
2023-12-15 15:42:26 -0300

    corrijo browerRouter

--- src/components/app/App.jsx
+++ src/components/app/App.jsx
@@ -1,16 +1,18 @@
 import './App.css'
 import { CharactersContextProvider } from '../../context/charactersContext' 
 import Characters from '../characters/Characters'
+import { BrowserRouter } from 'react-router-dom'
 
 function App() {
 
   return (
     <div className='container'>
       <h1 className='title'>Rick and Morty App</h1>
-      
-      <CharactersContextProvider>
-        <Characters />        
-      </CharactersContextProvider>
+      <BrowserRouter>
+        <CharactersContextProvider>
+          <Characters />        
+        </CharactersContextProvider>
+      </BrowserRouter>
 
     </div>
   )
